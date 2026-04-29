14d946843b3630f5b22d8659ed200422e8623b1d
Cainan Patterson <pattersoncainan@gmail.com>
2026-04-28 20:02:21 -0500

    Added Icons, Grid, Swapability.

--- app/src/main/java/com/example/odyss3y/MainActivity.java
+++ app/src/main/java/com/example/odyss3y/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.odyss3y;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +15,126 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    //Use these in the ancient style level
+    int[] ancientIcons = {
+            R.drawable.cave_dinosaur,
+            R.drawable.cave_mammoth,
+            R.drawable.caveman,
+            R.drawable.caveman_painting
+    };
+    //Use these in the medieval style level
+    int[] medIcons = {
+            R.drawable.chariot,
+            R.drawable.helmet,
+            R.drawable.shield,
+            R.drawable.vase
+    };
+    //Use these in the modern style level
+    int[] modernIcons = {
+            R.drawable.computer,
+            R.drawable.headphones,
+            R.drawable.phone,
+            R.drawable.calculator
+    };
+    //Use these in the future style level
+    int[] futureIcons = {
+            R.drawable.rocket,
+            R.drawable.sci_donut,
+            R.drawable.sci_eye,
+            R.drawable.sci_ficone
+    };
+    int widthOfTile, numOfTile = 8, widthOfScreen;
+    ArrayList<ImageView> icon = new ArrayList<>();
+    int iconToBeDragged, iconToBeReplaced;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainLayout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        widthOfScreen = displayMetrics.widthPixels;
+        int heightOfScreen = displayMetrics.heightPixels;
+        widthOfTile = widthOfScreen / numOfTile;
+        createBoard();
+        for (ImageView imageView : icon)
+        {
+            imageView.setOnTouchListener(new OnSwipeListener(this)
+            {
+                @Override
+                void onSwipeLeft() {
+                    super.onSwipeLeft();
+                    iconToBeDragged = imageView.getId();
+                    iconToBeReplaced = iconToBeDragged - 1;
+                    iconSwap();
+                }
+
+                @Override
+                void onSwipeRight() {
+                    super.onSwipeRight();
+                    iconToBeDragged = imageView.getId();
+                    iconToBeReplaced = iconToBeDragged + 1;
+                    iconSwap();
+                }
+
+                @Override
+                void onSwipeUp() {
+                    super.onSwipeUp();
+                    iconToBeDragged = imageView.getId();
+                    iconToBeReplaced = iconToBeDragged - numOfTile;
+                    iconSwap();
+                }
+
+                @Override
+                void onSwipeDown() {
+                    super.onSwipeDown();
+                    iconToBeDragged = imageView.getId();
+                    iconToBeReplaced = iconToBeDragged + numOfTile;
+                    iconSwap();
+                }
+            });
+        }
+    }
+
+    private void iconSwap()
+    {
+        int background = (int) icon.get(iconToBeReplaced).getTag();
+        int background1 = (int) icon.get(iconToBeDragged).getTag();
+        icon.get(iconToBeDragged).setImageResource(background);
+        icon.get(iconToBeReplaced).setImageResource(background1);
+        icon.get(iconToBeDragged).setTag(background);
+        icon.get(iconToBeReplaced).setTag(background1);
+    }
+    private void createBoard() {
+        GridLayout gridLayout = findViewById(R.id.board);
+        gridLayout.setRowCount(numOfTile);
+        gridLayout.setColumnCount(numOfTile);
+        gridLayout.getLayoutParams().width = widthOfScreen;
+        gridLayout.getLayoutParams().height = widthOfScreen;
+        for (int i = 0; i < numOfTile * numOfTile ; i++)
+        {
+            ImageView imageView = new ImageView(this);
+            imageView.setId(i);
+            imageView.setLayoutParams(new
+                    android.view.ViewGroup.LayoutParams(widthOfTile, widthOfTile));
+            imageView.setMaxHeight(widthOfTile);
+            imageView.setMaxWidth(widthOfTile);
+            int randomIcon = (int) Math.floor(Math.random() * ancientIcons.length);// Grabs random icons
+            imageView.setImageResource(ancientIcons[randomIcon]);
+            imageView.setTag(ancientIcons[randomIcon]);
+            icon.add(imageView);
+            gridLayout.addView(imageView);
+        }
     }
 }
---
+++ app/src/main/java/com/example/odyss3y/OnSwipeListener.java
@@ -0,0 +1,70 @@
+package com.example.odyss3y;
+
+import android.content.Context;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class OnSwipeListener implements View.OnTouchListener {
+    public GestureDetector gestureDetector;
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return gestureDetector.onTouchEvent(event);
+    }
+    public OnSwipeListener(Context context)
+    {
+        gestureDetector = new GestureDetector(context, new GestureListener());
+    }
+    private final class GestureListener extends GestureDetector.SimpleOnGestureListener
+    {
+        public static final int SWIPE_THRESHOLD = 100;
+        public static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+        @Override
+        public boolean onDown(MotionEvent e){
+            return true;
+        }
+
+        @Override
+        public boolean onFling(@Nullable MotionEvent e1, @NonNull MotionEvent e2, float velocityX, float velocityY) {
+            boolean result = false;
+            float yDiff = e2.getY() - e1.getY();
+            float xDiff = e2.getX() - e1.getX();
+            if (Math.abs(xDiff) > Math.abs(yDiff))
+            {
+                if (Math.abs(xDiff) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD)
+                {
+                    if (xDiff > 0)
+                    {
+                        onSwipeRight();
+                    }
+                    else
+                    {
+                        onSwipeLeft();
+                    }
+                    result = true;
+                }
+            }
+            else if (Math.abs(yDiff) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD)
+            {
+                if (yDiff > 0)
+                {
+                    onSwipeDown();
+                }
+                else
+                {
+                    onSwipeUp();
+                }
+                result = true;
+            }
+            return result;
+        }
+    }
+    void onSwipeLeft(){}
+    void onSwipeRight(){}
+    void onSwipeUp(){}
+    void onSwipeDown(){}
+}
